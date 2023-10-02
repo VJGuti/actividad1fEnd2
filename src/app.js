d2060269890e1588ef55cd3792f50217444b37d4
Víctor Gutiérrez <132306327+VJGuti@users.noreply.github.com>
2023-10-02 16:25:40 -0400

    Update app.js
    Modificando el app.js

--- src/app.js
+++ src/app.js
@@ -1,69 +1,112 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 // Componente Producto
-function Producto({ producto }) {
-  // Verifica que el producto tenga las propiedades necesarias
-  if (!producto || !producto.nombre) {
-    return <div className="error">Producto inválido</div>;
-  }
+class Producto extends React.Component {
+  render() {
+    const { producto, eliminarProducto, iniciarModificacion } = this.props;
 
-  return <div className="producto">{producto.nombre}</div>;
+    // Verifica que el producto tenga las propiedades necesarias
+    if (!producto || !producto.nombre) {
+      return <div className="error">Producto inválido</div>;
+    }
+
+    return (
+      <div className="producto">
+        {producto.nombre}
+        <button onClick={() => eliminarProducto(producto.id)}>Eliminar</button>
+        <button onClick={() => iniciarModificacion(producto)}>Modificar</button>
+      </div>
+    );
+  }
 }
 
 // Componente ListaProductos
-function ListaProductos({ productos, filtro }) {
-  // Verifica que los productos sean un array
-  if (!Array.isArray(productos)) {
-    return <div className="error">Los productos deben ser un array</div>;
-  }
+class ListaProductos extends React.Component {
+  render() {
+    const { productos, filtro, eliminarProducto, iniciarModificacion } = this.props;
 
-  return (
-    <div>
-      {productos
-        .filter(producto => producto.nombre.includes(filtro))
-        .map(producto => (
-          <Producto key={producto.id} producto={producto} />
-        ))}
-    </div>
-  );
+    // Verifica que los productos sean un array
+    if (!Array.isArray(productos)) {
+      return <div className="error">Los productos deben ser un array</div>;
+    }
+
+    return (
+      <div>
+        {productos
+          .filter(producto => producto.nombre.includes(filtro))
+          .map(producto => (
+            <Producto key={producto.id} producto={producto} eliminarProducto={eliminarProducto} iniciarModificacion={iniciarModificacion} />
+          ))}
+      </div>
+    );
+  }
 }
 
 // Componente App
-function App() {
-  const [productos, setProductos] = useState([
-    { id: 1, nombre: 'Producto 1' },
-    { id: 2, nombre: 'Producto 2' },
-    // Agrega más productos aquí
-  ]);
-  const [filtro, setFiltro] = useState('');
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productos: [
+        { id: 1, nombre: 'Producto 1' },
+        { id: 2, nombre: 'Producto 2' },
+        // Agrega más productos aquí
+      ],
+      filtro: '',
+      productoAModificar: null,
+    };
+  }
 
-  const agregarProducto = producto => {
+  agregarProducto = producto => {
     // Verifica que el producto sea válido antes de agregarlo
-    if (!producto  || !producto.nombre) {
+    if (!producto || !producto.nombre) {
       console.error('Intentaste agregar un producto inválido');
       return;
     }
 
-    setProductos([...productos, producto]);
+    this.setState(prevState => ({ productos: [...prevState.productos, producto] }));
   };
 
-  const buscarProducto = e => {
-    setFiltro(e.target.value);
+  eliminarProducto = id => {
+    this.setState(prevState => ({ productos: prevState.productos.filter(producto => producto.id !== id) }));
   };
 
-  return (
-    <Router>
-      <Switch>
-        <Route path="/productos">
-          <input type="text" onChange={buscarProducto} />
-          <ListaProductos productos={productos} filtro={filtro} />
-        </Route>
-        {/* Agrega más rutas aquí */}
-      </Switch>
-    </Router>
-  );
+  iniciarModificacion = producto => {
+    this.setState({ productoAModificar: producto });
+  };
+
+  modificarProducto = (id, nuevoNombre) => {
+    this.setState(prevState => ({
+      productos: prevState.productos.map(producto => (producto.id === id ? { ...producto, nombre: nuevoNombre } : producto)),
+      productoAModificar: null,
+    }));
+  };
+
+  buscarProducto = e => {
+    this.setState({ filtro: e.target.value });
+  };
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/productos">
+            <input type="text" onChange={this.buscarProducto} />
+            {this.state.productoAModificar ? (
+              <form onSubmit={e => { e.preventDefault(); this.modificarProducto(this.state.productoAModificar.id, e.target.producto.value); }}>
+                <input name="producto" defaultValue={this.state.productoAModificar.nombre} />
+                <button type="submit">Guardar cambios</button>
+              </form>
+            ) : null}
+            <ListaProductos productos={this.state.productos} filtro={this.state.filtro} eliminarProducto={this.eliminarProducto} iniciarModificacion={this.iniciarModificacion} />
+          </Route>
+          {/* Agrega más rutas aquí */}
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
